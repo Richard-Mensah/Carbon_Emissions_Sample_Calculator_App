8c88590dd42452ce4c5c6577201473507fbd504e
Richard Mensah <119437925+Richard-Mensah@users.noreply.github.com>
2023-11-10 22:27:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 
 #set page/use all #df is your dataframe
 st.set_page_config(layout="wide", page_title="Ecotiva Carbon Footprint")
